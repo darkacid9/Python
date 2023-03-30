76abc99e16221ccd7ab59a732a8f294c35a14661
darkacid9 <kaushikaman371@gmai.com>
2023-03-30 11:43:54 +0530

    new python file added

---
+++ main.py
@@ -0,0 +1,132 @@
+# print("i love pizza")
+# print('im going home')
+# print('im ok how about you')
+# first_name = "aman"
+# last_namen = "kaushik"
+# full_name = first_name +" "+last_namen
+# print("fuck off "+full_name)
+#
+# age = 24
+# age += 1
+# print("your age is " + str(age))
+# hight = 250.99
+# print("my hight is "+str(hight)
+#
+# human = True
+# print('are you a human ' +str(human))
+# print(type(human))
+# name, age, frome = "aman", 24, True
+# print(name)
+# print(age)
+# print(frome)
+#
+# aman = rahul = govind = 30
+# print(rahul)
+# print(aman)
+# print(govind)
+# name = "aman kaushik"
+# location = 'india'
+# print(len(name))
+# print(name.find('a'))
+# print(name.capitalize())
+# print(location.upper())
+# print(name.replace("a", "a"))
+# a = 1
+# b = 2.0
+# c = "3879"
+# b = int(b)
+# c = float(c)
+# print(a)
+# print(b)
+# print(c*678990)
+# name = input('what is your name?: ')
+# age = int(input("how old are you? "))
+# hight = float(input("how tall are you? "))
+# print('hello '+name)
+# print("you are "+str(age)+" years old ")
+# print("you are "+str(hight)+" cm tall")
+# age = int(input('how old are you '))
+#
+# if age == 100:
+#     print('you are century old!')
+# elif age >= 18:
+#     print('you are an adult!')
+#
+#     print('you haven`t born yet!'else  elif not(temp < 0 and temp > 30):
+#     print("you good to go out side!")
+#     print('enjoy your day!')
+#
+# while 1010==1010:
+#     print("fuck it!")
+#
+# name = ""
+# while len(name) ==0:
+# print("hey", 6 , 7 , 8 , sep="+", end= "")
+# print(" hey bro", end="")
+# print(" how was your day", end="")
+#
+# a = 45
+# b = 34
+# c = 244
+# print("the value of" ,c , "+" , 34 , "=" , a+b - c )
+# print("the value of" ,c , "-" , 34 , "=" , a-b  * c )
+# print("the value of" ,c , "*" , 34 , "=" , a*b  / c)
+# print("the value of" ,c , "/", 34 , "=" , a/b  + c)
+# print("the value of" ,c, "//" , 34 , "=" , a//b * c)
+# print("the value of" ,c , "//" , 34 , "=" , a//b - b)
+# a = "35"
+# b = "56"
+# print (int(a) + int(b))
+# print(float(a)+float(b))
+# string = '546'
+# numbers = 254647
+# string_numbers = float(string)
+# sum = numbers + string_numbers
+# print("the sum of string numbers is: " , sum)
+# a = input('what is your age')
+# print("nice your age is" , a )
+# b = input('whare are you frome')
+# print('nice you are from a nice place in india', b )
+# c = input('what is youer gender')
+# print('nice job youer full info is;',a +b +c )
+# a = input('number')
+# b = input('2nd number')
+# print(a+b)
+# a =  input('number: ')
+# b = input('2nd number: ')
+# print (float(a) - float(b))
+# name = "torry black"
+# for character in name:
+#     print(character)
+#
+# row = 'this is a game everyone can play'
+# rowlen = len(row)
+# print(rowlen)
+# name = 'aman kaushik ///////////'
+# print(name. upper())
+# print(name. lower())
+# print(name. strip('/'))
+# print(name. capitalize())
+# name = "the leant of control is"
+# print(len(name))
+# print(len(name.center(28)))
+# print(name.count("t"))
+#
+# str1 = "WelcomeToTheParty34"
+# print(str1.endswith('ty',0 , 20))
+# print(len(str1))
+# str1 = 'welcome to the party'
+# print(str1. startswith('w'))
+# print(str1.find("the"))
+#
+# print(str1.isalpha())
+# print(str1.isalnum())
+# print(str1.islower())
+# print(str1.isprintable())
+# print(str1.())
+# a = int(input('what is your age? '))
+# print('your age is:',a)
+# if (a>=18):
+#     print('you can drive')
+# else:
+#     print('you can not drive')
